1763c185e16ca7c013a65191deb6dc1f409c4596
HARI <103643955+Hariharan0001@users.noreply.github.com>
2023-09-06 23:44:03 +0530

    add api callback function

--- script.js
+++ script.js
@@ -47,32 +47,26 @@ function init() {
 
         }
         else {
+            var data;
             let x= req.toLowerCase();
-            var resq ={
-            "crop":"wheat,rice,blackgram,millet",
-            "wheat":"wheat can produce around 2.5 to 3.5 tons per hectare of land,the profit margin can be range from a few hundred to several thousand dollars.",
-            "rice":"On average, a hectare of land can produce around 5 to 8 tons of rice per season.",
-            "blackgram":" black gram yield can range from 400 to 800 kg per acre of land and a selling price of Rs. 150 per kg",
-             "millet":"millet can produce between 500 to 2000 kg per hectare",
-             "pesticides":"Insecticides,Herbicides,Fungicides,Rodenticides,Nematicides.",
-             "insecticides":"These are pesticides used to control insects that can damage crops, such as aphids, caterpillars, and beetles.",
-             "herbicides":"These are pesticides used to control weeds that compete with crops for nutrients and sunlight.",
-             "fungicides":"These are pesticides used to control fungal diseases that can affect crops, such as powdery mildew and rust.",
-             "rodenticides":"These are pesticides used to control rodents such as rats and mice that can cause damage to crops.",
-             "nematicides":"These are pesticides used to control nematodes, which are tiny worms that can cause damage to crops.",
-             "fertilizers":"Nitrogen fertilizers,Phosphorus fertilizers,Potassium fertilizers,Organic fertilizer,Micronutrient fertilizers.",
-             "nitrogen fertilizers":"These are fertilizers that contain nitrogen, which is essential for plant growth and development. Examples include ammonium nitrate, urea, and ammonium sulfate.",
-             "phosphorus fertilizers":"These are fertilizers that contain phosphorus, which is necessary for root development and flowering. Examples include triple superphosphate, diammonium phosphate, and monoammonium phosphate.",
-             "potassium fertilizers":"These are fertilizers that contain potassium, which is important for plant growth and stress tolerance. Examples include potassium chloride, potassium sulfate, and potassium nitrate.",
-             "organic fertilizers":"These are fertilizers that are derived from natural sources such as animal manure, compost, and bone meal. They provide a slow release of nutrients to plants and improve soil health.",
-             "micronutrient fertilizers":"These are fertilizers that contain micronutrients such as iron, zinc, and manganese, which are essential for plant growth and development. They are used in small quantities and are often added to other fertilizers."
-            }
-            if(resq[x]===undefined){
+            fet();
+            async function fet(){
+                let dataToSend={"key":`${x}`};
+                let response = await fetch('http://localhost:5000/getvalues', {
+                method: 'POST', // Use the HTTP method you need (e.g., POST)
+                headers: {
+                  'Content-Type': 'application/json' // Set the appropriate content type
+                },
+                body: JSON.stringify(dataToSend) // Convert the data to JSON and send it in the body
+              });
+          
+                data= await response.json();
+                 console.log(data[0].value);
+            if(!data[0].value){
                 var res="sorry the data is not relevent to agriculture❌";
-                
             }
             else{
-            var res = resq[x] ;
+            var res = data[0].value ;
             }
             let data_req = document.createElement('div');
             let data_res = document.createElement('div');
@@ -104,7 +98,7 @@ function init() {
                 scrollMsg.scrollTop = scrollMsg.scrollHeight;
             }
             scroll();
-
+        }
         }
     }
 
